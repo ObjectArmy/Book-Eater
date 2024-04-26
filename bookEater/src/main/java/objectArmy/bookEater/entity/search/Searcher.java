94e3496c49a8de7cb966a650ebeb97f905fbc29c
philipAthanasopoulos <filipposfivos@gmail.com>
2024-04-26 21:52:41 +0300

    working on profile settings

--- bookEater/src/main/java/objectArmy/bookEater/entity/search/Searcher.java
+++ bookEater/src/main/java/objectArmy/bookEater/entity/search/Searcher.java
@@ -1,6 +1,9 @@
 package objectArmy.bookEater.entity.search;
 
 import objectArmy.bookEater.entity.book.BookOffer;
+import objectArmy.bookEater.service.BookOfferService;
+import objectArmy.bookEater.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
--- bookEater/src/main/java/objectArmy/bookEater/entity/search/TitleSearcher.java
+++ bookEater/src/main/java/objectArmy/bookEater/entity/search/TitleSearcher.java
@@ -1,15 +1,21 @@
 package objectArmy.bookEater.entity.search;
 
 import objectArmy.bookEater.entity.book.BookOffer;
+import objectArmy.bookEater.service.BookOfferService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public class TitleSearcher implements Searcher{
+public class TitleSearcher implements Searcher {
+
+    @Autowired
+    private BookOfferService bookOfferService;
+
     @Override
     public List<BookOffer> search(String queryBookTitle) {
         /* For each book
-        *  if queryBookTile = bookTitle
-        *  Add it to the list */
+         *  if queryBookTile = bookTitle
+         *  Add it to the list */
         return List.of();
     }
 }
