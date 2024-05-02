faa15480282f85fc1db635e556b297f2166b1ffe
philipAthanasopoulos <filipposfivos@gmail.com>
2024-05-02 18:09:58 +0300

    added spring and DB annotations in all service tests

--- bookEater/src/test/java/objectArmy/bookEater/service/BookCategoryServiceTest.java
+++ bookEater/src/test/java/objectArmy/bookEater/service/BookCategoryServiceTest.java
@@ -1,9 +1,12 @@
 package objectArmy.bookEater.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class BookCategoryServiceTest {
 
     @Test
--- bookEater/src/test/java/objectArmy/bookEater/service/BookRequestServiceTest.java
+++ bookEater/src/test/java/objectArmy/bookEater/service/BookRequestServiceTest.java
@@ -2,9 +2,12 @@ package objectArmy.bookEater.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class BookRequestServiceTest {
 
     @BeforeEach
